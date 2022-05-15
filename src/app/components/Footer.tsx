07e9d2200dad47392133ac80ce0c94d7ca5a22df
Ricardo Costa <ricardo18acs@gmail.com>
2022-05-15 11:33:14 -0300

    feat: new maps and structures

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,8 +4,11 @@ export const Footer = () => {
   return (
     <div id="footer">
       <div className='animate__animated animate__pulse animate__infinite	infinite'>
-          Timer: <span className='score-value'>30s</span>
-        </div>
+        Timer: <span className='score-value'>30s</span>
+      </div>
+      <div id="power-by">
+        Power by: <a target="_blank" href="https://github.com/Ricardo-Costa">Ricardo-Costa</a>
+      </div>
     </div>
   )
 }
