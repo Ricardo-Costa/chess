d31bc5dbaea20db4dc4ba8e3dcf2f74345f74258
Ricardo Costa <ricardo18acs@gmail.com>
2022-05-16 22:19:43 -0300

    feat: import pawn action rules

--- src/app/utils/pawn-action.utils.ts
+++ src/app/utils/pawn-action.utils.ts
@@ -2,6 +2,7 @@ import BasePiece from "../models/pieces/base-piece";
 import { PawnBottom } from "../models/pieces/pawn-bottom";
 import { PawnTop } from "../models/pieces/pawn-top";
 
+// movements that the piece can do
 const specialMovementByPositionMap = {
   "A": [ "B" ],
   "B": [ "A", "C" ],
@@ -11,7 +12,18 @@ const specialMovementByPositionMap = {
   "F": [ "E", "G" ],
   "G": [ "F", "H" ],
   "H": [ "G" ],
-}
+};
+// movements that the piece do not should do
+const specialMovementByPositionForbiddenMap = {
+  "A": [ "A" ],
+  "B": [ "B" ],
+  "C": [ "C" ],
+  "D": [ "D" ],
+  "E": [ "E" ],
+  "F": [ "F" ],
+  "G": [ "G" ],
+  "H": [ "H" ],
+};
 
 const verifyCompatibility = (
   nextPosition: number,
@@ -25,9 +37,11 @@ const verifyCompatibility = (
   return availablePositions.filter( position => {
     const keyOfPosition = piece.getPosition()[0];
     const specialMovements: string[] = Reflect.get(specialMovementByPositionMap, keyOfPosition);
+    const specialMovementsForbidden: string[] = Reflect.get(specialMovementByPositionForbiddenMap, keyOfPosition);
 
     let specialPosition;
     const keepSpecialPositions = [], keepValidSpecialPositions = [];
+    // keep positions that this piece can go to event that place has um opponent piece
     for (const key of specialMovements) {
       specialPosition = `${key}${nextPosition}`;
 
@@ -35,11 +49,22 @@ const verifyCompatibility = (
 
       if (opponentPositions.includes(specialPosition))
         keepValidSpecialPositions.push(specialPosition);
-    }      
+    }
+    let specialPositionForbidden;
+    const keepSpecialPositionsForbidden = [], keepValidSpecialPositionsForbidden = [];
+    // keep positions that this piece can go to, if that place has um opponent piece
+    for (const key of specialMovementsForbidden) {
+      specialPositionForbidden = `${key}${nextPosition}`;
+
+      keepSpecialPositionsForbidden.push(specialPositionForbidden);
+
+      if (opponentPositions.includes(specialPositionForbidden))
+        keepValidSpecialPositionsForbidden.push(specialPositionForbidden);
+    }
     return (
       keepValidSpecialPositions.includes(position) ||
       !keepSpecialPositions.includes(position)
-    );
+    ) && !keepValidSpecialPositionsForbidden.includes(position);
   });
 }
 
